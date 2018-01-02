8bbf5bf5342f3d86b970ba761b04465e24f42243
Arthur Deguines <deguinesarthur@gmail.com>
2018-01-02 01:15:54 +0100

    osef

---
+++ src/PopupPerdu.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class PopupPerdu {
+	JFrame frame;
+	public PopupPerdu(){
+		frame = new JFrame();
+		frame.setBounds(600, 500, 200, 100);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+
+}
